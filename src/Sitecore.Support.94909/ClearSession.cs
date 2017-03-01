935925c149937fcc44c938f577a55af4a9b33ebc
DK\ALTU <altu@sitecore.net>
2017-03-01 15:43:30 +0200

    First attempt to push

---
+++ src/Sitecore.Support.94909/ClearSession.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Pipelines.LoggingIn;
+using Sitecore.Security.Accounts;
+using Sitecore.Shell.Applications.ContentEditor;
+using Sitecore.Web.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sitecore.Support.Pipelines.LoggingIn
+{
+    public class ClearSession
+    {
+        public void Process(LoggingInArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            User user = User.FromName(args.Username, false);
+
+            if (user != null)
+            {
+                DomainAccessGuard.Sessions.RemoveAll(p => p.UserName == user.Name);
+            }
+        }
+    }
+}
