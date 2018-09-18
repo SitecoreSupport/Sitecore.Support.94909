295e0818abe47ece6051e3366f6a1ed406d04bbc
Giacomo Portolan <gpo@sitecore.net>
2018-09-18 13:52:12 -0400

    Commit changes for patch

---
+++ src/Sitecore.Support.94909/RemoveMultipleSession.cs
@@ -0,0 +1,27 @@
+﻿using Sitecore.Pipelines.LoggedIn;
+using System;
+using System.Web;
+
+namespace Sitecore.Support
+{
+    public class RemoveMultipleSession
+    {
+          public void Process(LoggedInArgs args)
+          {
+                var coreDb = Data.Database.GetDatabase("core");
+                var tickets = coreDb.PropertyStore.GetPropertyKeys("SC_TICKET"); // take all the user login tickets
+                if (tickets != null)
+                {
+                    foreach (var ticket in tickets)
+                    {
+                        Web.Authentication.Ticket ticketByKey = Web.Authentication.Ticket.Parse(coreDb.PropertyStore.GetStringValue(ticket));
+                        if (string.Equals(Context.GetUserName(), ticketByKey.UserName) && !string.Equals(HttpContext.Current.Session.SessionID, ticketByKey.ClientId)) // remove the tickets for the same user name, but different ASP.Net session ID
+                        {
+                            Web.Authentication.DomainAccessGuard.Kick(ticketByKey.ClientId);
+                            Web.Authentication.TicketManager.RemoveTicket(ticketByKey.Id);
+                        }
+                    }
+                }
+          }
+    }
+}
