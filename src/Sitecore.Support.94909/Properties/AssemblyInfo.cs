eb3744407e233328de001a8dcbdd47becaa54b46
sc-JianMingSoon <jianming.soon@sitecore.com>
2019-10-04 15:54:12 +0800

    Add new fix for kick duplicated admin session

---
+++ src/Sitecore.Support.94909/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.94909")]
+[assembly: AssemblyProduct("Sitecore.Support.94909")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.94909/RemoveMultipleSession.cs
@@ -0,0 +1,52 @@
+﻿namespace Sitecore.Support
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Sitecore.Diagnostics;
+    using Sitecore.Pipelines.LoggedIn;
+    using Sitecore.Web.Authentication;
+    using System.Web;
+
+    class RemoveMultipleSession
+    {
+        #region original code
+        public void Process(LoggedInArgs args)
+        {
+            try
+            {
+                string userName = args.Username.ToLower();
+                string currentSessionID = HttpContext.Current.Session.SessionID;
+
+                // Using args.Username.ToLower() & s.Username.ToLower() due to capital issues
+                foreach (DomainAccessGuard.Session item in from s in DomainAccessGuard.Sessions
+                                                           where s.UserName.ToLower() == userName && s.SessionID != currentSessionID
+                                                           select s)
+                {
+                    string sessionID = item.SessionID;
+
+                    DomainAccessGuard.Kick(sessionID);
+                    List<string> list = HttpContext.Current.Application["SessionsToKick"] as List<string>;
+                    if (list == null)
+                    {
+                        list = new List<string>();
+                        HttpContext.Current.Application["SessionsToKick"] = list;
+                    }
+                    list.Add(sessionID);
+
+                    Log.Info($"The {sessionID} session of the {userName} user was kicked", (object)this);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.ToString(), ex, (object)this);
+            }
+        }
+        #endregion
+
+        public RemoveMultipleSession()
+        {
+
+        }
+    }
+}
